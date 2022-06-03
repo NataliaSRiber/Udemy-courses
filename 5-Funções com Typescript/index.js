92fdea17c5c4b4133fc4d0d3c0212b3e61ace299
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-06-03 09:15:21 -0300

    feat: adds section five

---
+++ 5-Funções com Typescript/index.js
@@ -0,0 +1,69 @@
+// introduction
+// 55 - funcções sem retorno - void
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+function noReturn() {
+    console.log("Esta função não tem retorno!");
+}
+noReturn();
+// 56 - callback como argumento
+function greeting(name) {
+    return "Ol\u00E1 ".concat(name, "!");
+}
+function preGreeting(f, userName) {
+    console.log("Preparando a função");
+    var greet = f(userName);
+    console.log(greet);
+}
+preGreeting(greeting, "Kalifa");
+preGreeting(greeting, "Momo");
+// 57 - generic Functions
+function firstElement(arr) {
+    return arr[0];
+}
+console.log(firstElement([1, 2, 3]));
+console.log(firstElement(["Ola", "você", "!"]));
+console.log(firstElement([{ 1: "voce" }, { 2: "quer" }, { 3: "chorar?" }]));
+function mergeObjects(obj1, obj2) {
+    return __assign(__assign({}, obj1), obj2);
+}
+var newObject = mergeObjects({ name: "Natalia" }, { sexo: "feminino" });
+console.log(newObject);
+// 58 - Constraints nas Generic Functions
+function biggestNumber(a, b) {
+    var biggest; // tem que mostrar q ele aceita tudo
+    if (+a > +b) {
+        biggest = a;
+    }
+    else {
+        biggest = b;
+    }
+    return biggest;
+}
+console.log(biggestNumber(5, 3));
+console.log(biggestNumber("15", "3"));
+// console.log(biggestNumber("5", 3));  nao aceita
+// 59 - Definindo tipos de parâmetros (na hora da execução)
+function mergeArrays(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+console.log(mergeArrays([5, 3], [2, 1]));
+console.log(mergeArrays(["15", "3"], [5, 3])); // define aqui - faz sentido utilizarmos quando estamos pegando uma função pronta. Se for uma função criada pela gente fasz sentido utilizar o exemplo anterior e ja definir no inicio
+// 60 - Parametros opcionais
+function modernGreeting(name, greet) {
+    if (greet) { // temos que fazer o if pois mesmo colocando ser opcional com ? ele da problema
+        return "Ol\u00E1 ".concat(greet, " ").concat(name, ", tudo bem?");
+    }
+    return "Ol\u00E1 ".concat(name, ", tudo bem?");
+}
+console.log(modernGreeting("Natalia"));
+console.log(modernGreeting("Natalia", "Sra"));
