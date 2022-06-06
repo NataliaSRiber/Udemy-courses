fd0d25df6fc49808d4571041f85be55eafc6a980
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-06-06 16:53:52 -0300

    feat: adds getById route

--- nest-do-zero/nest-course/src/controller/courses/courses.controller.ts
+++ nest-do-zero/nest-course/src/controller/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses') // mostra a rota a ser utilizada
 export class CoursesController {
   @Get() // podemos colocar a rota aqui tbm ou inserir rota aninhada
   findAll() {
     return 'Courses List';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Course id is ${id}`;
   }
 }
