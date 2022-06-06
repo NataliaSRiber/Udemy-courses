0a38d931d412a14f19bf0594c0ba272d10b2f98c
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-06-06 17:47:57 -0300

    feat: adds informations about status code

--- nest-do-zero/nest-course/src/controller/courses/courses.controller.ts
+++ nest-do-zero/nest-course/src/controller/courses/courses.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses') // mostra a rota a ser utilizada
 export class CoursesController {
   @Get() // podemos colocar a rota aqui tbm ou inserir rota aninhada
-  findAll() {
-    return 'Courses List';
+  findAll(@Res() response) {
+    return response.status(200).send;
   }
 
   @Get(':id')
@@ -13,7 +22,9 @@ export class CoursesController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.NO_CONTENT) // força o metodo a retornar o 204 nao o 201. Para resposta utilizamos outro
   create(@Body() body: object) {
+    // para uma info especifica no @Body(name), so retornara o name
     return body;
   }
 }
