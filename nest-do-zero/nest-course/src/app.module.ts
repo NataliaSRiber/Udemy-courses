472d87944a8e291c82c1d7857a9c923e4a61284a
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-06-07 16:05:43 -0300

    feat: changes app.module file to connect to database

--- nest-do-zero/nest-course/src/app.module.ts
+++ nest-do-zero/nest-course/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 
+// docker run --name postgres -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgress
+// docker start postgres
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      // no forroot para os parametros para se conectar com o banco
+      type: 'postgres', // tipo de banco de dados
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'docker',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
