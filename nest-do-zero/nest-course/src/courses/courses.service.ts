77fd2123ffa2384aaa087c1de8459e546d8bdab0
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-06-07 11:09:54 -0300

    feat: creates servicesfuncions

--- nest-do-zero/nest-course/src/courses/courses.service.ts
+++ nest-do-zero/nest-course/src/courses/courses.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do framework NestJS',
+      description: 'Fundamentos do framework NestJS',
+      tags: ['node.js', 'nest.js', 'javascript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    return this.courses.find((course) => course.id === Number(id));
+  }
+
+  create(createCourseDto: any) {
+    // dto usado para criação
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+  }
+}
