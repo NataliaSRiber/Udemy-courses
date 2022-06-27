922520081731580c528f512ac65881cc38a447da
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-06-27 12:17:11 -0300

    feat: adds constraints in generic

--- typescript_do_basico_avancado/7-Criação de Tipos/index.js
+++ typescript_do_basico_avancado/7-Criação de Tipos/index.js
@@ -1,6 +1,12 @@
-// Generics
+// 81 - Generics
 function showData(arg) {
     return "O dado \u00E9 ".concat(arg);
 }
 console.log(showData(5));
 console.log(showData('testando'));
+// 82 constraints in generic
+function showProductName(obj) {
+    return "O nome do produto \u00E9 ".concat(obj.name);
+}
+var obj1 = { name: "porta", cor: "branca" };
+console.log(showProductName(obj1));
--- typescript_do_basico_avancado/7-Criação de Tipos/index.ts
+++ typescript_do_basico_avancado/7-Criação de Tipos/index.ts
@@ -1,4 +1,4 @@
-// Generics
+// 81 - Generics
 
 function showData<T>(arg: T): string {
   return `O dado é ${arg}`
@@ -7,3 +7,12 @@ function showData<T>(arg: T): string {
 console.log(showData(5));
 console.log(showData('testando'));
 
+// 82 constraints in generic
+
+function showProductName<T extends {name: string}>(obj: T) { // precisa ser um objeto e conter a propriedade name
+  return `O nome do produto é ${obj.name}`
+}
+
+const obj1 = {name: "porta", cor: "branca"}
+
+console.log(showProductName(obj1));
