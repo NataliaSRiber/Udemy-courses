c65cf24ae098ac2b7acae1484d76ef984b51a159
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-07-04 10:19:48 -0300

    feat: adds template literals type

---
+++ typescript_do_basico_avancado/7-Criação_de_Tipos/index.js
@@ -0,0 +1,49 @@
+// 81 - Generics
+function showData(arg) {
+    return "O dado \u00E9 ".concat(arg);
+}
+console.log(showData(5));
+console.log(showData('testando'));
+// 82 constraints in generic
+function showProductName(obj) {
+    return "O nome do produto \u00E9 ".concat(obj.name);
+}
+var obj1 = { name: "porta", cor: "branca" };
+console.log(showProductName(obj1));
+var myCar = { name: "Ferrari", wheels: 4, engine: 7.0, color: "Branco" };
+var myPen = { name: "Bic", wheels: false, engine: false, color: "azul" };
+console.log(myCar);
+console.log(myPen);
+// 84 type parameters
+function getSomeKey(obj, key) {
+    return "A chave ".concat(key, " est\u00E1 presente no objeto e tmem o valor de ").concat(obj[key]);
+}
+var server = {
+    hd: '2TB',
+    ram: '32GB'
+};
+console.log(getSomeKey(server, 'ram'));
+function showCharName(obj, name) {
+    return "".concat(obj[name]);
+}
+var char = {
+    name: "Maggie Simpson",
+    age: 1,
+    hasDriveLicence: false
+};
+console.log(showCharName(char, 'name')); // espera a chave name
+// 86 typeof type operator
+var userName = "Matheus";
+var userName2 = "Jade";
+var userName4 = "Murilo";
+var newtruck = {
+    km: 10000,
+    kg: 5000,
+    description: "Caminhão para cargas pequenas"
+};
+function showKm(km) {
+    console.log("\u00D2 ve\u00EDculo tem a km de: ".concat(km));
+}
+showKm(newtruck.km);
+var someVar = 5;
+var testing = "some text";
---
+++ typescript_do_basico_avancado/7-Criação_de_Tipos/index.ts
@@ -0,0 +1,119 @@
+// 81 - Generics
+
+function showData<T>(arg: T): string {
+  return `O dado é ${arg}`
+}
+
+console.log(showData(5));
+console.log(showData('testando'));
+
+// 82 constraints in generic
+
+function showProductName<T extends {name: string}>(obj: T) { // precisa ser um objeto e conter a propriedade name
+  return `O nome do produto é ${obj.name}`
+}
+
+const obj1 = {name: "porta", cor: "branca"}
+
+console.log(showProductName(obj1));
+
+//83 - generic com interface
+
+interface MyObject<T, U, Q> {
+  name: string,
+  wheels: T,
+  engine: U,
+  color: Q,
+}
+
+type Car = MyObject<number, number, string>;
+type Pen = MyObject<boolean, boolean, string>;
+
+const myCar: Car = {name: "Ferrari", wheels: 4, engine: 7.0, color: "Branco"};
+const myPen: Pen = {name: "Bic", wheels: false, engine: false, color: "azul"};
+
+console.log(myCar);
+console.log(myPen);
+
+// 84 type parameters
+
+function getSomeKey<T, K extends keyof T>(obj: T, key: K) { // k é uma chave de T
+  return `A chave ${key} está presente no objeto e tmem o valor de ${obj[key]}`
+}
+
+const server = {
+  hd: '2TB',
+  ram: '32GB',
+}
+
+console.log(getSomeKey(server, 'ram'));
+
+// 85 keyof type operator
+
+type Character = { name: string, age: number, hasDriveLicence: boolean }
+
+type C = keyof Character
+
+function showCharName(obj: Character, name: C):string { // relaciona 
+  return `${obj[name]}`
+}
+
+const char: Character = {
+  name: "Maggie Simpson",
+  age: 1,
+  hasDriveLicence: false,
+}
+
+console.log(showCharName(char, 'name')); // espera a chave name
+
+// 86 typeof type operator
+
+const userName: string = "Matheus";
+const userName2: typeof userName = "Jade"
+// const userName3: typeof userName = 2;
+
+type x = typeof userName
+const userName4: x = "Murilo";
+
+// 87 indexed acces type
+
+type Truck = { km:number, kg: number, description: string }
+
+type km = Truck['km']; // tipo baseado nesta chave
+
+const newtruck: Truck = {
+  km: 10000,
+  kg: 5000,
+  description: "Caminhão para cargas pequenas"
+}
+
+function showKm(km: km) {
+  console.log(`Ò veículo tem a km de: ${km}`);
+}
+
+showKm(newtruck.km);
+
+// 88 conditional types
+
+interface A {
+}
+
+interface B extends A {}
+
+type myType = B extends A ? number: string
+
+const someVar: myType = 5;
+// const someVar2: myType = "ola";
+
+// 89 template literals type
+
+type testA = "text";
+type CustomType = `some ${testA}`;
+
+const testing: CustomType = "some text";
+
+type a1 = "Testando";
+type a2 = "Union";
+
+type a3 = `${a1}` | `${a2}`;
+
