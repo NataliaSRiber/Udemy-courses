61137451e98727213a28387c1dff23ae6cf59e18
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-08-15 15:30:38 -0300

    feat: function in components

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -5,6 +5,11 @@ const name: string = "Natalia";
 const year: number = 2022;
 const isWorking: boolean = true;
 
+// 2 - funções
+const userGreeting = (name: string): string => {
+  return `Olá, ${name}`;
+};
+
 function App() {
   return (
     <div className="App">
@@ -12,9 +17,9 @@ function App() {
       <h2>Nome: {name}</h2>
       <p>Ano: {year}</p>
       {isWorking && (
-        <div>
+        <><div>
           <p>Está trabalhando!</p>
-        </div>
+        </div><h3>{userGreeting(name)}</h3></>
       )}
     </div>
   );
