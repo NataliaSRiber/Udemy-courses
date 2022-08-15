76b2ecf4984e53fe0f2f85ee787398e1a94cbcd0
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-08-15 15:12:34 -0300

    feat: adds variables

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+// 1 - variáveis
+const name: string = "Natalia";
+const year: number = 2022;
+const isWorking: boolean = true;
+
 function App() {
   return (
     <div className="App">
       <h1>React com Typescript</h1>
+      <h2>Nome: {name}</h2>
+      <p>Ano: {year}</p>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando!</p>
+        </div>
+      )}
     </div>
   );
 }
