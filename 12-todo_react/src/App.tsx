e22f4fceb4bde7c9e85b75e15db5ba7fe7f3ed69
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-08-23 17:25:15 -0300

    docs: create header and footer

--- 12-todo_react/src/App.tsx
+++ 12-todo_react/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      TODO React
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ 12-todo_react/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Header () {
+  return (
+    <header>
+      <h1>React + TS TODO list</h1>
+    </header>
+  );
+}
