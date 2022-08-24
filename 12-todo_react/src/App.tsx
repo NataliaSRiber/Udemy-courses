8d35f8e7f56aadb07fbfcb0852d4b6178f5e0fa3
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-08-24 15:20:37 -0300

    feat: calls TaskList and TaskForm on App.jsx

--- 12-todo_react/src/App.tsx
+++ 12-todo_react/src/App.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import styles from './App.module.css'
+import TaskForm from './components/TaskForm/TaskForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className={styles.main}>
-        <h1>Conteudo...</h1>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText='Criar Tarefa'/>
+        </div>
+        <div>
+          <h2>Suas Tarefas:</h2>
+          <p>Lista</p>
+        </div>
       </main>
       <Footer />
     </div>
