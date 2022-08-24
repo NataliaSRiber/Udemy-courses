129abdb8ccb470f75a8e4ce44d23fe5016ee3b80
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-08-24 10:37:02 -0300

    docs: modifies App.tsx file

--- 12-todo_react/src/App.tsx
+++ 12-todo_react/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className={styles.main}>
+        <h1>Conteudo...</h1>
+      </main>
       <Footer />
     </div>
   );
