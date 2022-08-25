64072861d5d0628b6f9697fc13087edce646bbfa
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-08-25 14:24:00 -0300

    feat: creates TaskForm css

--- 12-todo_react/src/components/TaskForm/TaskForm.tsx
+++ 12-todo_react/src/components/TaskForm/TaskForm.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styles from './TaskForm.module.css'
 
 export interface IAppProps {
   btnText: string
@@ -6,12 +7,12 @@ export interface IAppProps {
 
 function TaskForm ({btnText}: IAppProps) {
   return (
-    <form>
-      <div>
+    <form className={styles.form}>
+      <div className={styles.input_container}>
         <label htmlFor='title'>Título:</label>
         <input type='text' name='title' placeholder='Título da Tarefa' />
       </div>
-      <div>
+      <div className={styles.input_container}>
         <label htmlFor='difficulty'>Dificuldade:</label>
         <input type='text' name='difficulty' placeholder='Dificuldade da Tarefa' />
       </div>
