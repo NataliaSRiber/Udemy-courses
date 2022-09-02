8a1c91f6d7c5d757f3129cd7a486a88e5ccb8b84
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-09-02 14:49:39 -0300

    feat: creates handleChanges function, also add states for title and difficulty level

--- 12-todo_react/src/components/TaskForm/TaskForm.tsx
+++ 12-todo_react/src/components/TaskForm/TaskForm.tsx
@@ -1,20 +1,50 @@
 import * as React from 'react';
-import styles from './TaskForm.module.css'
+import styles from './TaskForm.module.css';
 
 export interface IAppProps {
   btnText: string
 }
 
 function TaskForm ({btnText}: IAppProps) {
+
+  const [id, setId] = React.useState<number>(0);
+  const [title, setTitle] = React.useState<string>('');
+  const [difficulty, setDifficult] = React.useState<number>(0);
+ 
+  const addTaskHandler = () => {
+
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if(e.target.name === "title") {
+      setTitle(e.target.value) 
+     } else {
+      setDifficult(parseInt(e.target.value)) // dado do input é uma string e queremos um numero
+    }
+  }
+  console.log(title);
+  console.log(difficulty);
+  
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={addTaskHandler} className={styles.form}>
       <div className={styles.input_container}>
         <label htmlFor='title'>Título:</label>
-        <input type='text' name='title' placeholder='Título da Tarefa' />
+        <input 
+          type='text' 
+          name='title' 
+          placeholder='Título da Tarefa'
+          onChange={handleChange}
+        />
       </div>
       <div className={styles.input_container}>
         <label htmlFor='difficulty'>Dificuldade:</label>
-        <input type='text' name='difficulty' placeholder='Dificuldade da Tarefa' />
+        <input 
+          type='text' 
+          name='difficulty' 
+          placeholder='Dificuldade da Tarefa' 
+          onChange={handleChange}
+        />
       </div>
       <input type='submit' value={btnText}/>
     </form>
