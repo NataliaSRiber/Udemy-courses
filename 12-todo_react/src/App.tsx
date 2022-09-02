b6e1cf3fb296eb7972be02b9f50f8ac486f0e750
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-09-02 16:05:16 -0300

    feat: adds as props taskList on app.ts

--- 12-todo_react/src/App.tsx
+++ 12-todo_react/src/App.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import styles from './App.module.css'
 import TaskForm from './components/TaskForm/TaskForm';
+import { ITask } from './interfaces/Task';
+
+
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]); // lista de tarefas
+
   return (
     <div className="App">
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar Tarefa'/>
+          <TaskForm 
+            btnText='Criar Tarefa' 
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         </div>
         <div>
           <h2>Suas Tarefas:</h2>
