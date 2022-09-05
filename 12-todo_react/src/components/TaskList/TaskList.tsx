d9e5792163f15a7df9d4f6eaf1580ac2d93c9a91
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-09-05 15:48:14 -0300

    feat: adds css style on TaskList

--- 12-todo_react/src/components/TaskList/TaskList.tsx
+++ 12-todo_react/src/components/TaskList/TaskList.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ITask } from '../../interfaces/Task';
+import styles from './TaskList.module.css';
 
 export interface IAppProps {
   taskList: ITask[];
@@ -10,12 +11,12 @@ function TaskList ({taskList}: IAppProps) {
     <>
       {taskList.length > 0 ? (
         taskList.map((task)=> (
-          <div key={task.id}>
-            <div>
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
               <h4>{task.title}</h4>
               <p>Dificuldade: {task.difficulty}</p>
             </div>
-            <div>
+            <div className={styles.actions}>
               <i className='="bi bi-pencil'></i>
               <i className='="bi bi-trash'></i>
             </div>
